57f844df6e5412338a25b1be4a88e23842d42ac8
Flavio Silva <flavio.silvapinkman@gmail.com>
2023-08-31 13:15:20 -0300

    log

--- ourAnimals/ourAnimals/Program.cs
+++ ourAnimals/ourAnimals/Program.cs
@@ -163,6 +163,7 @@ do
         break;
 
         case "2":
+
             // Add a new animal friend to the ourAnimals array
 
             string anotherPet = "y";
@@ -186,7 +187,129 @@ do
              Console.WriteLine($"We currently have {petCount} pets that need homes. We can manage {(maxPets - petCount)} more.");
 
             }
-            while(anotherPet == "y" && petCount < maxPets) 
+
+            bool validEntry = false;
+
+            do
+            {
+                // get species (cat or dog) - string animalSpecies is a required field 
+                Console.WriteLine("\n\rEnter 'dog' or 'cat' to begin a new entry");
+                readResult = Console.ReadLine();
+
+                if(readResult != null) 
+                {
+                    animalSpecies = readResult.ToLower();
+
+                    if (animalSpecies != "dog" && animalSpecies != "cat")
+                    {
+                        validEntry = false;
+                    }
+                    else
+                    {
+                        validEntry = true;
+                    }
+
+                }
+
+            } while (validEntry == false);
+
+            // build the animal the ID number - for example C1, C2, D3 (for Cat 1, Cat 2, Dog 3)
+
+            animalID = animalSpecies.Substring(0, 1) + (petCount+ 1).ToString();
+
+            // get the pet's age. can be ? at initial entry. 
+
+            do 
+            {
+                int petAge;
+
+                Console.WriteLine("Enter the pet's age or enter ? if unknown");
+                readResult = Console.ReadLine();
+
+                if (readResult != null) 
+                {
+                    animalAge = readResult;
+
+                    if (readResult != "?")
+                    {
+                        validEntry = int.TryParse(animalAge, out petAge);
+                    }
+                    else
+                    {
+                        validEntry = true;
+                    }
+                }
+
+            } while(validEntry == false);
+
+
+            // get a description of the pet's physical appearance/condition - animalPhysicalDescription can be blank.
+
+            do
+            {
+                Console.WriteLine("Enter a physical description of the pet (size, color, gender, weight, housebroken)");
+                readResult = Console.ReadLine();
+
+                if(readResult != null) 
+                {
+                    animalPhysicalDescription = readResult.ToLower();
+
+                    if(animalPhysicalDescription == "") 
+                    {
+                        animalPhysicalDescription = "tbd";
+                    }
+                }
+
+
+            } while (animalPhysicalDescription == "");
+
+            // get a description of the pet's personality - animalPersonalityDescription can be blank.
+
+            do
+            {
+
+                Console.WriteLine("Enter a description of the pet's personality (likes or dislikes, tricks, energy level)");
+                readResult = Console.ReadLine();
+                if(readResult != null) 
+                { 
+                    animalPersonalityDescription = readResult.ToLower();
+
+                    if(animalPersonalityDescription == "") 
+                    {
+                        animalPersonalityDescription = "tbd";
+                    }
+                }
+
+            } while (animalPersonalityDescription == "");
+
+            // get the pet's nickname. animalNickname can be blank.
+
+            do
+            {
+                Console.WriteLine("Enter a nickname for the pet");
+                readResult = Console.ReadLine();
+
+                if( readResult != null)
+                {
+                    animalNickname = readResult.ToLower();
+
+                    if(animalNickname == "") 
+                    {
+                        animalNickname = "tbd";
+                    }
+                }
+
+            } while (animalNickname == "");
+
+            // store the pet information in the ourAnimals array (zero based)
+            ourAnimals[petCount, 0] = "ID #: " + animalID;
+            ourAnimals[petCount, 1] = "Species: " + animalSpecies;
+            ourAnimals[petCount, 2] = "Age: " + animalAge;
+            ourAnimals[petCount, 3] = "Nickname: " + animalNickname;
+            ourAnimals[petCount, 4] = "Physical description: " + animalPhysicalDescription;
+            ourAnimals[petCount, 5] = "Personality: " + animalPersonalityDescription;
+
+            while (anotherPet == "y" && petCount < maxPets) 
             {
                 // increment petCount (the array is zero-based, so we increment the counter after adding to the array)
 
